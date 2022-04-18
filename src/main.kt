a47e02393addf3a16bbd7339e386ff03a40d8c44
Bruno <bruno.jpinto@outlook.com>
2022-04-18 19:49:44 -0300

    Aula 03 - Criando novas contas

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,34 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    
+
+    val contaCorrente = ContaCorrente(
+        titular = "Bruninho",
+        numero = 15452
+    )
+
+    val contaPoupanca = ContaPoupanca (
+        titular = "Bruninho 2",
+        numero = 58687
+    )
+
+    contaCorrente.deposita(1000.0)
+    contaPoupanca.deposita(1000.0)
+
+    println("Saldo corrente: ${contaCorrente.saldo}")
+    println("Saldo poupança: ${contaPoupanca.saldo}")
+
+    contaCorrente.saca(100.0)
+    contaPoupanca.saca(100.0)
+
+    println("Saldo corrente após saque: ${contaCorrente.saldo}")
+    println("Saldo poupança após saque: ${contaPoupanca.saldo}")
+
+    contaPoupanca.transfere(200.0, contaCorrente)
+    println("Saldo corrente após transferência: ${contaCorrente.saldo}")
+
+    contaCorrente.transfere(100.0, contaPoupanca)
+    println("Saldo poupança após transferência: ${contaPoupanca.saldo}")
+
 }
 
 
